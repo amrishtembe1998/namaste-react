b08bcebbd4c6c7232d68b54cf05c47005d5eaf2c
amrishtembe1998 <amrishtembe1998@gmail.com>
2024-01-21 11:36:04 +0530

    Added react-router-dom features & header pages

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import Header from "./Header";
+
+const About = () => {
+  return (
+    <>
+      <div>
+        <h1>About us</h1>
+        <h3>
+          This is Namaste React course. Trying to learn React concepts using
+          this project.
+        </h3>
+        <a href="/">Go back to homepage</a>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,20 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>This is contact us page.</h1>
+      <h2>
+        You can contact me on
+        <ul>
+          <li>phone no: +91-9404001254</li>
+          <li>email: amrishtembe1998@gmail.com</li>
+          <li>LinkedIn: https://www.linkedin.com/in/amrish-tembe-8913b2171</li>
+        </ul>
+      </h2>
+      <h4>
+        <a href="/">Go back to homepage</a>
+      </h4>
+    </div>
+  );
+};
+
+export default Contact;
